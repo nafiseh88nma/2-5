7fdd4a01e65f1c82e2ca185e099f5d906406919c
nafiseh <fun101.nm@gmail.com>
2020-01-29 13:36:55 +0330

    homework 2-5

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created by admin on 10/5/2019.
+ */
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println();
+        Scanner scan= new Scanner (System.in);
+        String a= new String();
+        a=scan.nextLine();
+
+    }
+}
